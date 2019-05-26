a326aeae9bd6045de0b36ded705d2eae85b9d2e0
everan-1994 <everan@aliyun.com>
2019-05-26 23:12:57 +0800

    food show h5

---
+++ js/tabs.js
@@ -0,0 +1,53 @@
+var searchImg = document.getElementById("search-icon");
+var maskWrap = document.getElementById("mask-wrap");
+
+var tabs = document.getElementsByClassName('tab-menu')[0].getElementsByTagName('li'),
+    contents = document.getElementById('content').getElementsByClassName('tab');
+
+// 高亮tab的选项卡
+(function changeTab(tab) {
+    for (var i = 0, len = tabs.length; i < len; i++) {
+        tabs[i].onmouseover = showTab;
+    }
+})();
+
+function showTab() {
+    for (var i = 0, len = tabs.length; i < len; i++) {
+        if (tabs[i] === this) {
+            tabs[i].className = 'tab-active';
+            contents[i].style.display = 'block';
+        } else {
+            tabs[i].className = '';
+            contents[i].style.display = 'none';
+        }
+    }
+}
+
+// 获取输入框的值，隐藏搜索按钮
+
+function getSearchName() {
+    searchImg.style.display = "none";
+}
+
+// 点击删除按钮，触发删除按钮的模态框
+
+function delHistory() {
+    maskWrap.style.display = "block";
+}
+
+// 点击确认按钮，删除历史记录
+function hiddenMask() {
+    maskWrap.style.display = "none";
+}
+
+// 点击搜索按钮回车事件
+function getSearch() {
+    var searchValue = document.getElementById("searchValue").value; // 搜索框输入的值
+    var emptyWrap = document.getElementsByClassName("empty-wrap"); // 搜索空的结果的展示
+    var contentWrap = document.getElementById("content"); // 搜索有结果的节点
+    if (searchValue) {
+        emptyWrap[0].style.display = "block";
+        contentWrap.style.display = "none";
+    }
+
+}
---
+++ readme.txt
@@ -0,0 +1,36 @@
+文件说明：
+
+html文件说明：
+
+    aboutUs.html 关于我们文件
+    brand.html 品牌文件
+    brandCooperation.html 品牌合作文件
+    brandDetail.html 品牌详情
+    contactUs.html 联系我们
+    contactUsForm.html 联系我们form 表单
+    cooperation.html 商超合作
+    cooperationDetail.html 商超合作详情
+    index.html 首页
+    search.html 搜索页面
+
+
+css 文件说明：
+    html文件名和css文件名一致
+
+    额外说明：
+        accordion.css 为折叠菜单样式
+        certify.css 为首页轮播图样式
+        common.css 为抽取公共样式
+        reset.css 为重置样式
+        swiper.min.css 为轮播插件样式
+
+js 文件说明：
+    accordion.js 为折叠菜单js代码
+    menu.js 为手机侧边栏滑动导航
+    swiper.min.js 为swiper 轮播插件代码
+    tabs.js 为tab选项卡切换js代码
+    video.js 为视频截图js代码
+
+
+相关插件地址：
+    轮播插件地址：https://www.swiper.com.cn/
